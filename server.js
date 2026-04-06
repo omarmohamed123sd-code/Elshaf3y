5642532e5bcbb5053ebe9dea89a83c366dba8443
omarmohamed123sd-code <omarmohamed123sd@gmail.com>
2026-04-06 11:28:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,14 @@ const app = express();
 
 app.use(express.json());
 
-const users = [
-    { username: "admin", password: "1234" }
-];
-
-app.post("/login", (req, res) => {
-    const { username, password } = req.body;
-
-    const user = users.find(u => 
-        u.username === username && u.password === password
-    );
-
-    if (user) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
-});
-
+// test route
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server is working 🔥");
 });
 
+// مهم جدًا 👇
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
+    console.log("Server running on port " + PORT);
 });
